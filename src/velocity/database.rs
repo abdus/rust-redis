2cf9198b6e3e2a6818bafe2ee53356beb641971d
Abdus <abdus@abdus.net>
2023-11-19 23:49:03 +0530

    feat: single instance for database
    using mutex and lazy

--- src/velocity/database.rs
+++ src/velocity/database.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use once_cell::sync::Lazy;
+use std::{collections::HashMap, error::Error, sync::Mutex, vec};
 
 /*
  * redis primarily have these five data-types:
@@ -14,25 +15,71 @@ pub enum DataTypes {
     String(String),
 }
 
+impl Clone for DataTypes {
+    fn clone(&self) -> Self {
+        match self {
+            DataTypes::String(s) => DataTypes::String(s.clone()),
+        }
+    }
+}
+
 #[derive(Debug)]
-pub struct Database {
+struct Database {
     data: HashMap<String, DataTypes>,
 }
 
-impl Database {
-    pub fn get_instance() -> Database {
-        Database {
-            data: HashMap::new(),
+unsafe impl Sync for Database {}
+
+// persistant db variable through out the program
+static mut DB: Lazy<Mutex<Database>> = Lazy::new(|| {
+    Mutex::new(Database {
+        data: HashMap::new(),
+    })
+});
+
+#[derive(Debug)]
+pub struct DatabaseOps;
+
+impl DatabaseOps {
+    pub fn set(&mut self, key: String, value: DataTypes) {
+        let mut db = unsafe { DB.lock().unwrap() };
+
+        db.data.insert(key, value);
+        println!("Database: {:#?}", db.data);
+    }
+
+    pub fn get(&self, key: String) -> Option<DataTypes> {
+        let db = unsafe { DB.lock().unwrap() };
+
+        let key = &key[..];
+        let data = db.data.get(key);
+
+        let owned_data = data.clone();
+
+        match owned_data {
+            Some(data) => Some(data.clone()),
+            None => None,
         }
     }
 
-    pub fn set(&mut self, key: String, value: DataTypes) {
-        self.data.insert(key, value);
-        println!("Database: {:?}", self.data);
+    pub fn keys(&self) -> Vec<String> {
+        let db = unsafe { DB.lock().unwrap() };
+        let keys = db.data.keys();
+        let mut keys_list: Vec<String> = vec![];
+
+        for key in keys {
+            keys_list.push(key.to_string());
+        }
+
+        keys_list
     }
 
-    pub fn get(&self, key: String) -> Option<&DataTypes> {
+    pub fn del(&self, key: String) -> Option<DataTypes> {
+        let mut db = unsafe { DB.lock().unwrap() };
         let key = &key[..];
-        self.data.get(key)
+
+        let removed_key = db.data.remove(key);
+
+        removed_key
     }
 }
